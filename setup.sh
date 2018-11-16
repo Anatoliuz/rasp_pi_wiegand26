43f33dde00a14bead1031b4c3e2d2c7494f4815e
Anatoliuz <afilinovs@gmail.com>
2018-11-16 17:48:10 +0300

    finalochka 2

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
+gcc -Wall -pthread -ggdb3 -lpigpiod_if2 -lyuarel source.c mongoose.* encoding_* -o ITPRFIDWiegandController
+sudo yes | cp -rf ITPRFIDWiegandController /home/pi/bin
 sudo systemctl enable pigpiod.service
 sudo sudo cp wiegand.service /etc/systemd/system/wiegand.service
-systemctl start wiegand.service
-systemctl enable wiegand.service
+sudo systemctl enable wiegand.service
+sudo systemctl start wiegand.service
